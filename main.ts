a4f156342ea427c4965f2c21bec5473240e922fc
Marcos Candeia <marrcooos@gmail.com>
2025-05-08 17:01:29 -0300

    Adds request log
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ import build from "./bundler.ts";
 const app = new Hono();
 
 app.post("/*", async (c) => {
-  return c.json(await build(await c.req.json()));
+  console.log("REQUEST");
+  return c.res = c.json(await build(await c.req.json()));
 });
 
 Deno.serve(app.fetch);
