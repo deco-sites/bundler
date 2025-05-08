72dcd4ea27d3b7fba939983405fd5f1e73d0fd2b
Marcos Candeia <marrcooos@gmail.com>
2025-05-08 16:51:01 -0300

    Adds await to build
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import build from "./bundler.ts";
 const app = new Hono();
 
 app.post("/*", async (c) => {
-  return c.json(build(await c.req.json()));
+  return c.json(await build(await c.req.json()));
 });
 
 Deno.serve(app.fetch);
