f1d1c33327b10c21ea35549648403071851cef3d
Marcos Candeia <marrcooos@gmail.com>
2025-05-08 18:30:25 -0300

    Adds error response
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- main.ts
+++ main.ts
@@ -5,7 +5,14 @@ const app = new Hono();
 const portEnv = Deno.env.get("PORT");
 const port = portEnv ? +portEnv : 8000;
 app.post("/*", async (c) => {
-  return c.res = c.json(await build(await c.req.json()));
+  try {
+    return c.res = c.json(await build(await c.req.json()));
+  } catch (e) {
+    if (e instanceof Error) {
+      return c.res = c.json({ error: e.message }, 500);
+    }
+    return c.res = c.json({ error: "Unknown error" }, 500);
+  }
 });
 
 Deno.serve({
